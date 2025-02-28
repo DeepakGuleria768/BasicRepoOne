83ee9db51e50342509d22071563e000f2c7eb522
Deepak Guleria <dguleria07@gmail.com>
2025-02-28 20:55:41 +0530

    Commit

--- src/main/kotlin/ExtentionFunctions.kt
+++ src/main/kotlin/ExtentionFunctions.kt
@@ -1,16 +1,15 @@
-
-
 // used to add new function in the existing class without actually updating that class code
 
-// fun class_name . your own fun name (){
+// fun class_name. your own fun name (){
 
 //}
 
-fun String.MyStringFunction():String{
+fun String.MyStringFunction(): String {
     return "hello peoples"
 }
 
-val data : String = "hello"
+val data: String = "hello"
+
 // here you are adding your own function without actually change the class String code
 val result = data.MyStringFunction()
 fun main() {
